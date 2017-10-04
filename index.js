dc6284b1d43f6c1303445f0bc97ce2719afd4ad9
Ravi Verma <raviverma2695@yahoo.com>
2017-10-04 09:28:10 +0530

    Hi

--- index.js
+++ index.js
@@ -31,14 +31,7 @@ app.get('/webhook/', function (req, res) {
 })
 
 
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'niec') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
+
 
 // Spin up the server
 app.listen(app.get('port'), function() {

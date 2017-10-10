2bf0db625b4cc23807ea9ac5b9fe5a8798ea67f4
Ravi Verma <raviverma2695@yahoo.com>
2017-10-10 07:08:57 +0530

    wwo

--- index.js
+++ index.js
@@ -39,7 +39,12 @@ const wwoApiKey = '7eabf4ed0b8949368ac13125171010';
 
 app.post('/weatherfetch',function(req,res) {
 	
-	
+	response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response 
+  //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
 });
 
 app.post('/webhook', function (req, res) {

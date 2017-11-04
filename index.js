4af3324ab8fdab22471c7ac00ba658e7e07c9d4b
Ravi Verma <raviverma2695@yahoo.com>
2017-11-04 11:10:00 +0530

    date time

--- index.js
+++ index.js
@@ -30,23 +30,22 @@ const host = 'api.worldweatheronline.com';
 const wwoApiKey = '7eabf4ed0b8949368ac13125171010';
 
 app.post('/weatherWebhook',function(req,res) {
-  // Get the city and date from the request
- // let city = req.body.result.parameters['geo-city']; // city is a required param
+  
   let city = req.body.result.parameters.address['city'];
-  // Get the date for the weather forecast (if present)
+ 
   let date = '';
-  //console.log('ddd' +req.body.result.parameters['date-time']);
+ 
   if (req.body.result.parameters['date-time']) {
     date = req.body.result.parameters['date-time'];
     console.log('Date: ' + date);
   }
-  // Call the weather API
+  
   callWeatherApi(city, date).then((output) => {
-    // Return the results of the weather API to API.AI
+    
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
   }).catch((error) => {
-    // If there is an error let the user know
+    
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
   });
@@ -55,28 +54,33 @@ app.post('/weatherWebhook',function(req,res) {
 
 function callWeatherApi (city, date) {
   return new Promise((resolve, reject) => {
-    // Create the path for the HTTP request to get the weather
+   
     let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
       '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
     console.log('API Request: ' + host + path);
-    // Make the HTTP request to get the weather
+    
     http.get({host: host, path: path}, (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
+      let body = ''; 
+      res.on('data', (d) => { body += d; }); 
       res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
+        
         let response = JSON.parse(body);
         let forecast = response['data']['weather'][0];
         let location = response['data']['request'][0];
         let conditions = response['data']['current_condition'][0];
         let currentConditions = conditions['weatherDesc'][0]['value'];
-        // Create response
-        let output = `Current conditions in the ${location['type']} 
-        ${location['query']} are ${currentConditions} with a projected high of
-        ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
-        ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
+       
+        // let output = `Current conditions in the ${location['type']} 
+       // ${location['query']} are ${currentConditions} with a projected high of
+       // ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
+       // ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
+       // ${forecast['date']}.`;
+		
+		
+		let output = `Current conditions in the ${location['type']} 
+        ${location['query']} are ${currentConditions} with temperature ${conditions['temp_C']} c on 
         ${forecast['date']}.`;
-        // Resolve the promise with the output text
+       
         console.log(output);
         resolve(output);
       });
